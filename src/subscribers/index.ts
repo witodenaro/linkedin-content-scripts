81e155ccbd936ee96796dc49f48ede1574bfbe58
Vitali Litvinovich <vitali@healthera.co.uk>
2024-07-07 18:02:18 +0200

    main – make subscription use dynamic imports

--- src/subscribers/index.ts
+++ src/subscribers/index.ts
@@ -1,12 +1,11 @@
-import "./addConnection";
-import './firstConnections'
-import "./getGeolocations";
-import "./getProfile";
-import "./getCurrentUserProfile";
-import "./removeConnection";
-import "./searchPeople";
-import './getConnectionsCount'
-
 export async function subscribe() {
+  import("./addConnection");
+  import("./firstConnections");
+  import("./getGeolocations");
+  import("./getProfile");
+  import("./getCurrentUserProfile");
+  import("./removeConnection");
+  import("./searchPeople");
+  import("./getConnectionsCount");
   return true;
 }
